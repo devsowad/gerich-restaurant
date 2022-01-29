428e40d65903767d2b2f476aece4a670628cd717
Sowad <sowad1035@gmail.com>
2022-01-29 21:03:08 +0600

    init

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+const App: React.FC = () => {
+  return (
+    <div>
+      <Navbar />
+      <Header />
+      <AboutUs />
+      <SpecialMenu />
+      <Chef />
+      <Intro />
+      <Laurels />
+      <Gallery />
+      <FindUs />
+      <Footer />
+    </div>
+  );
+};
+
+export default App;
